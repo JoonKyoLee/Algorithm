e9a666cb34f093401fcf05db2e7268202cb82e9a
이준교 <51353601+JoonKyoLee@users.noreply.github.com>
2025-05-24 01:13:49 +0900

    [Bronze III] Title: 오븐 시계, Time: 148 ms, Memory: 15992 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int hour = Integer.parseInt(st.nextToken());
+        int minute = Integer.parseInt(st.nextToken());
+        int time = Integer.parseInt(br.readLine());
+        int minute_result = minute + time;
+
+        minute = minute_result % 60;
+        hour += minute_result / 60;
+
+        if(hour >= 24){
+            hour -= 24;
+        }
+
+        System.out.println(hour + " " + minute);
+    }
+}
