1b35734d9d8eebbfcb03d8934797a39a912816d2
이준교 <51353601+JoonKyoLee@users.noreply.github.com>
2025-06-04 19:29:09 +0900

    [Bronze V] Title: A+B - 8, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,5 +1,6 @@
-T = int(input())
+N = int(input())
 
-for i in range(T):
+for i in range(N):
     A, B = map(int, input().split())
-    print("Case #{}: {} + {} = {}".format(i + 1, A, B, A + B))
+    C = A + B
+    print(f"Case #{i + 1}: {A} + {B} = {C}")
