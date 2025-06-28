a12061462e72b356b594f2da35a9193e668a6e46
이준교 <51353601+JoonKyoLee@users.noreply.github.com>
2025-06-28 23:16:31 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 112140 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+N, M = map(int, input().split())
+
+maze = []
+
+for _ in range(N):
+    maze.append(list(map(int, input())))
+
+def bfs_shortest_path(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < M and maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y] + 1
+                queue.append((nx, ny))
+
+    return maze[N - 1][M - 1]
+
+print(bfs_shortest_path(0, 0))
