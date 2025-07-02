8393ce2c9f90ecc39f1a635ce955ebbb5e032edc
이준교 <51353601+JoonKyoLee@users.noreply.github.com>
2025-07-02 23:40:04 +0900

    [Silver II] Title: 최대 힙, Time: 168 ms, Memory: 114200 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline())
+hq = []
+
+for _ in range(N):
+    num = int(sys.stdin.readline())
+
+    if num == 0:
+        if not hq:
+            print(0)
+        else:
+            print(-heapq.heappop(hq))
+    else:
+        heapq.heappush(hq, -num)
