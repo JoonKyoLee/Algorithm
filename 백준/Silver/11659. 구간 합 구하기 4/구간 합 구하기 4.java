f9a8db4711fb9aad75b3f9a2eba1af3e011f0ed4
이준교 <51353601+JoonKyoLee@users.noreply.github.com>
2025-10-30 22:29:24 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 656 ms, Memory: 55492 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<Integer> partSum = new ArrayList<>();
+        partSum.add(0);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            partSum.add(partSum.get(i) + Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            bw.write((partSum.get(end) - partSum.get(start - 1)) + "\n");
+        }
+
+        bw.flush();
+    }
+}
