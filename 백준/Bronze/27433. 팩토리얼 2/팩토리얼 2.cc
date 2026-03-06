ff27f9f9e8ac310587bf129e15b5ef4128268d99
이준교 <51353601+JoonKyoLee@users.noreply.github.com>
2026-03-06 22:46:56 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long factorial(long long n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int N;
+    cin >> N;
+    cout << factorial(N) << "\n";
+    return 0;
+}
