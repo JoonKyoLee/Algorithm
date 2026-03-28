d509e3d67aee4e1e9414d03f0fe425940c24736f
이준교 <51353601+JoonKyoLee@users.noreply.github.com>
2026-03-29 01:47:10 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1);
+    dp[1] = 1;
+    if (n >= 2)
+        dp[2] = 3;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = ((dp[i - 2] * 2) % 10007 + dp[i - 1] % 10007) % 10007;
+    }
+
+    cout << dp[n] << "\n";
+}
